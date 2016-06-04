2f84e7d635be3c8c4535e0a98a2f605555198256
Alexander Baxter <Alexander Baxter>
2016-06-04 21:12:33 +0200

    Merge camera and current enemy updates

---
+++ Assets/Scripts/Archer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Archer : MonoBehaviour {
+
+	public Player player1;
+	public Player player2;
+
+	public GameObject arrow;
+
+	public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void Init(Player m_player1, Player m_player2){
+		player1 = m_player1;
+		player2 = m_player2;
+	}		
+
+	Vector3 playerDirection(){
+		float player1_distance = (player1.transform.position - transform.position).magnitude;
+		float player2_distance = (player2.transform.position - transform.position).magnitude;
+		Player closest_player;
+		if (player1_distance < player2_distance){
+			closest_player = player1;
+		}
+		else{
+			closest_player = player2;
+		}
+		Vector3 directionVector = (closest_player.transform.position - transform.position).normalized;
+		return directionVector;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += playerDirection() * movementSpeed;
+	}
+}
