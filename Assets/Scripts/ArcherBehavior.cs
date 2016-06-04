b674ab4728da886529a3d66e7342a15385f1ef84
JohanLaine <johan.laine@gmail.com>
2016-06-04 16:13:51 +0200

    First attempt at making an archer that shoots arrows

---
+++ Assets/Scripts/ArcherBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcherBehavior : MonoBehaviour {
+
+	public Player player1;
+
+	public Transform arrow;
+
+	float m_cooldown = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	Vector3 player1Direction(){
+		Vector3 directionVector;
+		directionVector = (player1.transform.position - transform.position).normalized;
+		return directionVector;
+	}
+
+	void spawnArrow(){
+		Instantiate(arrow, transform.position, Quaternion.identity);
+		//new_arrow.position = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(player1Direction());
+		//Debug.Log(transform.rotation);
+		transform.position += Vector3.left * 0.1f;
+		if (m_cooldown < 0f){
+			Debug.Log(player1Direction());
+			spawnArrow();
+			m_cooldown = 1f;
+		}
+
+		m_cooldown -= Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/ArrowBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBehavior : MonoBehaviour {
+
+	public Vector3 startPosition;
+	// Use this for initialization
+
+	public void Init(Vector3 startPosition){
+		startPosition = startPosition;
+	}
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
